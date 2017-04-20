19a86334a798a09713b49a2d682d714962a66d39
dorisol1019 <dorisol@hotmail.co.jp>
2017-04-21 06:36:32 +0900

    Delete Dust.

--- SNS_Bot/Program.cs
+++ SNS_Bot/Program.cs
@@ -23,18 +23,7 @@ namespace tweetBot
             var twitterBot = CharacterBot.Create(CharacterType.Yarimizu_Moe, PlatFormType.Twitter, DataTableType.DataBase);
 
             var mastodonBot = CharacterBot.Create(CharacterType.Yarimizu_Moe, PlatFormType.Mastodon, DataTableType.DataBase);
-
-#if DEBUG
-            //mastodonbot.RegularPostAsync().Wait();
-            //mastodonbot.FollowBackAndGreetingAsync().Wait();
-
-            //mastodonbot.RetweetRelationTagAsync().Wait();
-            //mastodonBot.FollowBackAndGreetingAsync().Wait();
-            //mastodonBot.SendMessageToFollowerAsync(new TsuraiWords(),SrtifType.Ganbare).Wait();
-            //return;
-            //mastodonbot.SendTodayWetherAsync().Wait();
-
-#endif
+            
             switch (argments)
             {
                 case "RegularTweet":
@@ -66,30 +55,6 @@ namespace tweetBot
 
             Console.WriteLine("success.");
         }
-
-        static public void Seed(tweetBot.Context.SerifDataContext context)
-        {
-
-            var serifCSV = CSVRead.getSerifCSV(
-        "D:/リポジトリ的な何か/SerifExcerpter/Get_on_SerifExcerpter/Get_on_SerifExcerpter/萌瑛.csv");
-
-
-
-            foreach (var serifData in serifCSV)
-            {
-                context.Serifs.AddOrUpdate(p => p.Id,
-                new Models.SerifData
-                {
-                    Id = serifData.Id,
-                    Name = serifData.Name,
-                    Text = serifData.Text,
-                    LastUsedTime = null,
-                    Type = serifData.Type
-                });
-            }
-
-
-            context.SaveChanges();
-        }
+        
     }
 }
